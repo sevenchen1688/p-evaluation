d3f8df4f4b5212d9667853b6ff8e4dd258ea37de
chenzl <chenzl@ligeit.com>
2020-09-16 14:38:01 +0800

    初始化提交

---
+++ evaluation-main/src/main/java/p/evaluation/main/EvaluationMainApplication.java
@@ -0,0 +1,13 @@
+package p.evaluation.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationMainApplication.class, args);
+    }
+
+}
---
+++ evaluation-main/src/test/java/p/evaluation/main/EvaluationMainApplicationTests.java
@@ -0,0 +1,13 @@
+package p.evaluation.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
